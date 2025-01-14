3ecd30cbbf7fffc37703e53f5819c743dbcb56a2
Michael Lozitckii <worldspawn.web@gmail.com>
2025-01-14 15:57:51 +0300

    feat: 🎸 first tutorial steps + merge field

--- airtest/Hell Merge/android-googlechrome-1080x2400.air/android-googlechrome-1080x2400.py
+++ airtest/Hell Merge/android-googlechrome-1080x2400.air/android-googlechrome-1080x2400.py
@@ -28,4 +28,21 @@ sleep(10.0)
 wait(Template(r"tpl1736852766557.png", record_pos=(-0.002, -0.039), resolution=(1080, 2400)))
 assert_exists(Template(r"tpl1736852766557.png", record_pos=(-0.002, -0.039), resolution=(1080, 2400)), "First Tutorial Window.")
 
+# Skip Dialog
+touch(Template(r"tpl1736858679598.png", record_pos=(0.324, -0.858), resolution=(1080, 2400)))
+sleep(7.0)
+touch(Template(r"tpl1736858679598.png", record_pos=(0.324, -0.858), resolution=(1080, 2400)))
 
+# Touch before Tutorial Pointer
+sleep(7.0)
+wait(Template(r"tpl1736858768883.png", record_pos=(-0.005, -0.041), resolution=(1080, 2400)))
+touch(Template(r"tpl1736858777773.png", record_pos=(0.002, -0.025), resolution=(1080, 2400)))
+
+# Tutorial Pointer
+sleep(5.0)
+assert_exists(Template(r"tpl1736858804654.png", record_pos=(0.369, 0.688), resolution=(1080, 2400)), "Tutorial Finger")
+touch(Template(r"tpl1736858804654.png", record_pos=(0.369, 0.688), resolution=(1080, 2400)))
+
+# Merge Field Exists
+sleep(5.0)
+assert_exists(Template(r"tpl1736858868805.png", record_pos=(0.001, 0.051), resolution=(1080, 2400)), "Merge Field")
