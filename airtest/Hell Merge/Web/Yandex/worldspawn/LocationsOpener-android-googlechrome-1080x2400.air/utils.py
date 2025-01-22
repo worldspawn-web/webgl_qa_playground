5d8c14cef9fd5a9ae45dab91b019a8b88de18fcc
Michael Lozickii <worldspawn.web@gmail.com>
2025-01-22 14:01:24 +0300

    perf: additional checker for auto win utility
    - Больше не делает лишний раз свайпы, если есть кнопка "Cheats"

--- airtest/Hell Merge/Web/Yandex/worldspawn/LocationsOpener-android-googlechrome-1080x2400.air/utils.py
+++ airtest/Hell Merge/Web/Yandex/worldspawn/LocationsOpener-android-googlechrome-1080x2400.air/utils.py
@@ -15,18 +15,21 @@ def interstitial_check():
 def autowin_toggle():
     # Enable Auto-Win Cheat
     touch((76, 454))
-
-    ## Cheat Opener Combination
-    for i in range(3):
-        swipe((318, 863), (318, 953))
-        sleep(1.0)
-    for i in range(2):
-        swipe((358, 908), (276, 908))
-        sleep(1.0)
-    for i in range(2):
-        swipe((276, 908), (358, 908))
-        sleep(1.0)
     
+    if not exists(Template(r"tpl1737543351937.png", record_pos=(-0.294, -0.45), resolution=(1080, 2400))):
+        ## Cheat Opener Combination
+        for i in range(3):
+            swipe((318, 863), (318, 953))
+            sleep(1.0)
+        for i in range(2):
+            swipe((358, 908), (276, 908))
+            sleep(1.0)
+        for i in range(2):
+            swipe((276, 908), (358, 908))
+            sleep(1.0)
+    else:
+        touch(Template(r"tpl1737543351937.png", record_pos=(-0.294, -0.45), resolution=(1080, 2400)))
+
     wait(Template(r"tpl1737374734015.png", record_pos=(0.005, -0.061), resolution=(1080, 2400)))
     touch(Template(r"tpl1737374741818.png", rgb=True, record_pos=(-0.097, -0.422), resolution=(1080, 2400)))
     sleep(1.0)
