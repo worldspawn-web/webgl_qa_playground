fb7bc29e2654340daac1e61cb82d918e2beba595
Michael Lozickii <worldspawn.web@gmail.com>
2025-01-27 17:50:34 +0300

    refactor: UIChecks (iOS)

--- airtest/Hell Merge/Web/Yandex/worldspawn/UIChecksLevel4-iOS-Chrome-1080x2400.air/UIChecksLevel4-iOS-Chrome-1080x2400.py
+++ airtest/Hell Merge/Web/Yandex/worldspawn/UIChecksLevel4-iOS-Chrome-1080x2400.air/UIChecksLevel4-iOS-Chrome-1080x2400.py
@@ -23,19 +23,22 @@ def main():
     # Sometimes a Shop could be opened on game start
     quit_window()
     
+    play_btn = Template(r"tpl1737988742063.png", record_pos=(0.394, 0.506), resolution=(828, 1792))
+
+    
     # Close Popup Offer
     if exists(Template(r"tpl1736958318809.png", record_pos=(-0.002, -0.025), resolution=(1080, 2400))):
         touch(Template(r"tpl1736958340836.png", record_pos=(0.436, -0.674), resolution=(1080, 2400)))
         sleep(1.0)
-
+        if not exists(play_btn):
+            interstitial_check()
 
     
     # UI Checks
-    assert_exists(Template(r"tpl1736958356734.png", record_pos=(-0.002, -0.52), resolution=(1080, 2400)), "Current Location Panel.")
+    location_panel = Template(r"tpl1736958356734.png", record_pos=(-0.002, -0.52), resolution=(1080, 2400))
+    assert_exists(location_panel, "Current Location Panel.")
     sleep(1.0)
     assert_exists(Template(r"tpl1736958972213.png", record_pos=(-0.364, 0.442), resolution=(1080, 2400)), "New Location is Available to Open.")
-    sleep(1.0)
-    assert_exists(Template(r"tpl1736959001633.png", record_pos=(-0.429, -0.549), resolution=(1080, 2400)), "NoADS Offer Icon.")
     sleep(1.0)
     assert_exists(Template(r"tpl1736959017731.png", record_pos=(-0.092, -0.191), resolution=(1080, 2400)), "Completed Quest Icon.")
     sleep(1.0)
@@ -51,50 +54,51 @@ def main():
     sleep(1.0)
 
     # Settings Window
-    touch(Template(r"tpl1736959343843.png", record_pos=(0.439, -0.683), resolution=(1080, 2400)))
+    touch(Template(r"tpl1737987990143.png", record_pos=(0.446, -0.664), resolution=(828, 1792)))
     wait(Template(r"tpl1736959353467.png", record_pos=(0.002, -0.048), resolution=(1080, 2400)))
     assert_exists(Template(r"tpl1736959386934.png", record_pos=(-0.26, -0.123), resolution=(1080, 2400)), "Settings Icons.")
+    
+    flag_ru = Template(r"tpl1736959402954.png", record_pos=(0.239, -0.016), resolution=(1080, 2400))
+    flag_ru_w = Template(r"tpl1736959956034.png", record_pos=(0.146, -0.184), resolution=(1080, 2400))
+    flag_us = Template(r"tpl1736959516722.png", record_pos=(0.236, -0.022), resolution=(1080, 2400))
+    flag_us_w = Template(r"tpl1736960052871.png", record_pos=(-0.143, -0.184), resolution=(1080, 2400))
 
-    if not exists(Template(r"tpl1736959402954.png", record_pos=(0.239, -0.016), resolution=(1080, 2400))):
-        assert_exists(Template(r"tpl1736959516722.png", record_pos=(0.236, -0.022), resolution=(1080, 2400)), "English Language Icon.")
-        touch(Template(r"tpl1736959516722.png", record_pos=(0.234, -0.016), resolution=(1080, 2400)))
+    if not exists(flag_ru):
+        assert_exists(flag_us, "English Language Icon.")
+        touch(flag_us)
         sleep(2.0)
-        assert_exists(Template(r"tpl1736959944935.png", record_pos=(-0.002, -0.056), resolution=(1080, 2400)), "Please fill in the test point.")
-        assert_exists(Template(r"tpl1736960023923.png", record_pos=(-0.145, -0.187), resolution=(1080, 2400)), "Please fill in the test point.")
-        touch(Template(r"tpl1736959956034.png", record_pos=(0.146, -0.184), resolution=(1080, 2400)))
+        assert_exists(Template(r"tpl1736959944935.png", record_pos=(-0.002, -0.056), resolution=(1080, 2400)), "Languages Panel.")
+        assert_exists(Template(r"tpl1736960023923.png", record_pos=(-0.145, -0.187), resolution=(1080, 2400)), "Current Language is English.")
+        touch(flag_ru_w)
         sleep(1.0)
-        assert_exists(Template(r"tpl1736960039427.png", record_pos=(0.149, -0.188), resolution=(1080, 2400)), "Please fill in the test point.")
-        touch(Template(r"tpl1736960052871.png", record_pos=(-0.143, -0.184), resolution=(1080, 2400)))
+        assert_exists(Template(r"tpl1736960039427.png", record_pos=(0.149, -0.188), resolution=(1080, 2400)), "Current Language is Russian.")
+        touch(flag_us_w)
         sleep(1.0)
-        touch(Template(r"tpl1736960057993.png", record_pos=(0.296, -0.35), resolution=(1080, 2400)))
-        interstitial_check()
+        quit_window()
     else:
-        assert_exists(Template(r"tpl1736959402954.png", record_pos=(0.239, -0.016), resolution=(1080, 2400)), "Russian Language Icon.")
-        touch(Template(r"tpl1736959402954.png", record_pos=(0.238, -0.019), resolution=(1080, 2400)))
+        assert_exists(flag_ru, "Russian Language Icon.")
+        touch(flag_ru)
         sleep(1.0)
-        assert_exists(Template(r"tpl1736960254062.png", record_pos=(-0.002, -0.051), resolution=(1080, 2400)), "Please fill in the test point.")
-        assert_exists(Template(r"tpl1736960244233.png", record_pos=(0.151, -0.19), resolution=(1080, 2400)), "Please fill in the test point.")
-        touch(Template(r"tpl1736960265512.png", record_pos=(-0.143, -0.184), resolution=(1080, 2400)))
+        assert_exists(Template(r"tpl1736960254062.png", record_pos=(-0.002, -0.051), resolution=(1080, 2400)), "Languages Panel.")
+        assert_exists(Template(r"tpl1736960244233.png", record_pos=(0.151, -0.19), resolution=(1080, 2400)), "Current Language is Russian.")
+        touch(flag_us_w)
         sleep(1.0)
-        assert_exists(Template(r"tpl1736960274150.png", record_pos=(-0.143, -0.187), resolution=(1080, 2400)), "Please fill in the test point.")
-        touch(Template(r"tpl1736960285216.png", record_pos=(0.148, -0.187), resolution=(1080, 2400)))
+        assert_exists(Template(r"tpl1736960274150.png", record_pos=(-0.143, -0.187), resolution=(1080, 2400)), "Current Language is English.")
+        touch(flag_ru_w)
         sleep(1.0)
-        touch(Template(r"tpl1736960057993.png", record_pos=(0.299, -0.349), resolution=(1080, 2400)))
-        interstitial_check()
+        quit_window()
 
-    touch(Template(r"tpl1736960580158.png", record_pos=(0.304, -0.356), resolution=(1080, 2400))) # quits settings menu *note for clearance*
-    interstitial_check()
+    quit_window() # quits settings menu *note for clearance*
 
     # NoADs Offer
-    assert_exists(Template(r"tpl1736959001633.png", record_pos=(-0.429, -0.549), resolution=(1080, 2400)), "NoAds Offer Icon.")
-    touch(Template(r"tpl1736959001633.png", record_pos=(-0.425, -0.545), resolution=(1080, 2400)))
-    sleep(3.0)
+    noads_btn = Template(r"tpl1736959001633.png", record_pos=(-0.429, -0.549), resolution=(1080, 2400))
+    assert_exists(noads_btn, "NoAds Offer Icon.")
+    touch(noads_btn)
+    sleep(2.0)
     assert_exists(Template(r"tpl1736960781169.png", record_pos=(-0.002, -0.028), resolution=(1080, 2400)), "NoAds Offer.")
     assert_exists(Template(r"tpl1736960864932.png", record_pos=(0.0, 0.178), resolution=(1080, 2400)), "NoAds Icons.")
     assert_exists(Template(r"tpl1736960904008.png", record_pos=(0.002, 0.505), resolution=(1080, 2400)), "Purchase Button.")
-    assert_exists(Template(r"tpl1736961036433.png", record_pos=(0.44, -0.673), resolution=(1080, 2400)), "Please fill in the test point.")
-    touch(Template(r"tpl1736961036433.png", record_pos=(0.437, -0.678), resolution=(1080, 2400)))
-    interstitial_check()
+    quit_window()
     
     # Shop Checker
     touch(Template(r"tpl1737035149615.png", record_pos=(-0.276, 0.591), resolution=(1080, 2400)))
@@ -105,8 +109,7 @@ def main():
     assert_exists(Template(r"tpl1737035234160.png", record_pos=(-0.299, 0.347), resolution=(1080, 2400)), "'Day Specials' Decor (which means the container is rendered).")
     assert_exists(Template(r"tpl1737036156830.png", record_pos=(0.013, -0.267), resolution=(1080, 2400)), "Icons on 'Day Specials'")
     assert_exists(Template(r"tpl1737036166170.png", record_pos=(0.001, -0.13), resolution=(1080, 2400)), "'Day Specials' Prices.")
-    assert_exists(Template(r"tpl1737036131630.png", record_pos=(0.006, 0.014), resolution=(1080, 2400)), "'On Sale' Container.")
-    # We cant check insides of a sale container, since it's randomly generated
+    assert_exists(Template(r"tpl1737036131630.png", record_pos=(0.006, 0.014), resolution=(1080, 2400)), "'On Sale' Container.") # We cant check insides of a sale container, since it's randomly generated
     
     swipe((0.5, 0.5), (0.5, 0.14))
     sleep(5.0)
@@ -127,18 +130,16 @@ def main():
     assert_exists(Template(r"tpl1737036670924.png", record_pos=(0.001, 0.065), resolution=(1080, 2400)), "Soft Icons #1")
     assert_exists(Template(r"tpl1737036678344.png", record_pos=(-0.003, 0.426), resolution=(1080, 2400)), "Soft Icons #2")
     sleep(1.0)
-    assert_exists(Template(r"tpl1737036690530.png", record_pos=(0.427, -0.631), resolution=(1080, 2400)), "Quit Window Button.")
-    touch(Template(r"tpl1737036690530.png", record_pos=(0.429, -0.634), resolution=(1080, 2400)))
-    interstitial_check()
+    quit_window()
     assert_exists(Template(r"tpl1737036727773.png", record_pos=(-0.002, -0.02), resolution=(1080, 2400)), "Main Scene.")
     
     # Current Location Panel Action
-    touch(Template(r"tpl1737039446656.png", record_pos=(0.007, -0.514), resolution=(1080, 2400)))
+    touch(location_panel)
     sleep(1.0)
     assert_exists(Template(r"tpl1737039458524.png", record_pos=(-0.003, -0.324), resolution=(1080, 2400)), "Current Location Extended Panel.")
-    touch(Template(r"tpl1737039446656.png", record_pos=(0.007, -0.514), resolution=(1080, 2400)))
+    touch(location_panel)
     sleep(1.0)
-    assert_exists(Template(r"tpl1736958356734.png", record_pos=(-0.002, -0.52), resolution=(1080, 2400)), "Minimized Location Panel.")
+    assert_exists(location_panel, "Minimized Location Panel.")
     
     # Player Info Window
     touch(Template(r"tpl1737039678341.png", record_pos=(-0.43, -0.687), resolution=(1080, 2400)))
@@ -146,8 +147,7 @@ def main():
     assert_exists(Template(r"tpl1737039698690.png", record_pos=(-0.206, -0.269), resolution=(1080, 2400)), "Current Level Icon.")
     assert_exists(Template(r"tpl1737039704372.png", record_pos=(0.005, 0.269), resolution=(1080, 2400)), "Locked LevelUp Button.")
     assert_exists(Template(r"tpl1737039714601.png", record_pos=(0.004, 0.042), resolution=(1080, 2400)), "Awards for the Next Level.")
-    touch(Template(r"tpl1737039720525.png", record_pos=(0.312, -0.457), resolution=(1080, 2400)))
-    interstitial_check()
+    quit_window()
 
     
     # New Zone Unlock
--- airtest/Hell Merge/Web/Yandex/worldspawn/UIChecksLevel4-iOS-Chrome-1080x2400.air/utils.py
+++ airtest/Hell Merge/Web/Yandex/worldspawn/UIChecksLevel4-iOS-Chrome-1080x2400.air/utils.py
@@ -15,11 +15,13 @@ def random_touch():
 # Checks for Interstitial
 # Call after every window close on the main scene!
 def interstitial_check():
-    sleep(3.0)
     if (exists(Template(r"tpl1736872848834.png", record_pos=(0.42, -0.967), resolution=(1080, 2400)))):
         touch(Template(r"tpl1736872848834.png", record_pos=(0.42, -0.967), resolution=(1080, 2400)))
     if exists(Template(r"tpl1736852577544.png", record_pos=(0.427, -0.967), resolution=(1080, 2400))):
         touch(Template(r"tpl1736852577544.png", record_pos=(0.427, -0.967), resolution=(1080, 2400)))
+# TODO: think about the way to capture this rare inter ad with white button
+#     if exists(Template(r"tpl1737988644552.png", rgb=True, record_pos=(-0.004, 0.82), resolution=(828, 1792))):
+#         touch(Template(r"tpl1737988644552.png", rgb=True, record_pos=(-0.004, 0.82), resolution=(828, 1792)))
         
 def quit_window():
     logger.info("Looking for a window to close...")
@@ -28,10 +30,10 @@ def quit_window():
         Template(r"tpl1737381782191.png", record_pos=(0.306, -0.533), resolution=(1080, 2400)),
         Template(r"tpl1737382243343.png", record_pos=(0.304, -0.436), resolution=(1080, 2400)),
         Template(r"tpl1737389486120.png", record_pos=(0.322, -0.673), resolution=(1080, 2400)),
-        Template(r"tpl1736958340836.png", record_pos=(0.436, -0.674), resolution=(1080, 2400))
+        Template(r"tpl1737988266220.png", record_pos=(0.44, -0.65), resolution=(828, 1792))
     ]
 
-    for _ in range(5):
+    for _ in range(2):
         cross_found = False
         for cross in cross_templates:
             if exists(cross):
@@ -41,7 +43,7 @@ def quit_window():
         if not cross_found:
             sleep(0.5)
             
-    logger.warning("No window to close was found after 5 attempts.")
+    logger.warning("No window to close was found after 10 attempts.")
     return False
 
 __all__ = ['random_touch', 'interstitial_check', 'quit_window']
